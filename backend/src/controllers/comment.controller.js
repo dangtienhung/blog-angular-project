d81f3f4fc553eb105a0b9853bd34ba0c7a3b8d31
B1001 <giab8185@gmail.com>
2023-06-04 21:27:52 +0700

    listed comment

--- backend/src/controllers/comment.controller.js
+++ backend/src/controllers/comment.controller.js
@@ -2,8 +2,6 @@ import commentsModel from '../models/comments.model.js';
 import Post from '../models/posts.model.js';
 import User from '../models/users.models.js';
 import CommentValidate from '../validates/comment.validate.js';
-import Post from '../models/posts.model.js';
-import commentsModel from '../models/comments.model.js';
 
 export const getComments = async (req, res) => {
   try {
